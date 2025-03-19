4a188a56ed8bac161aa07e25abf632d5202c7a9c
chernyaev_nikolay <chernyaev_nikolay@cybersec74.ru>
2025-03-19 13:48:49 +0500

    Add HMAC to be able to validate hashes

--- app/usecase/hasher/general.go
+++ app/usecase/hasher/general.go
@@ -1,26 +1,45 @@
 package hasher
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"strconv"
 	"unhashService/entity"
 	"unhashService/pkg/logger"
 )
 
 type UseCase struct {
-	log *logger.Logger
+	log        *logger.Logger
+	HMACSecret string
 }
 
-func New(log *logger.Logger) *UseCase {
+func New(log *logger.Logger, HMACSecret string) *UseCase {
 	return &UseCase{
-		log: log,
+		log:        log,
+		HMACSecret: HMACSecret,
 	}
 }
 
 var _ HasherUC = (*UseCase)(nil)
 
+// Вспомогательная функция для генерации HMAC
+func generateHMAC(phoneNumber, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(phoneNumber))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// Вспомогательная функция для создания строки перед хешированием
+func prepareDataForHashing(phoneNumber, secret string) string {
+	hmacHash := generateHMAC(phoneNumber, secret)
+	return fmt.Sprintf("%s:%s", phoneNumber, hmacHash)
+}
+
 // HashPhoneNumber принимает структуру []entity.Hash и возвращает слайс "хэшей" номера телефона.
 func (uc *UseCase) HashPhoneNumber(hashes []entity.PhoneNumber, domain string) ([]string, error) {
+
 	domainInt, err := strconv.ParseInt(domain, 10, 64)
 	if err != nil {
 		uc.log.Info("Error parsing domain while hashing: " + err.Error())
@@ -29,6 +48,7 @@ func (uc *UseCase) HashPhoneNumber(hashes []entity.PhoneNumber, domain string) (
 	hashedData := make([]string, 0, len(hashes))
 
 	for _, h := range hashes {
+		h.PhoneNumber = prepareDataForHashing(h.PhoneNumber, uc.HMACSecret)
 		hashBytes := []byte(h.PhoneNumber)
 
 		hashedBytes := make([]byte, len(hashBytes))
